8fbeaf11fa6c09cceda95a64df34a2f7fbe8477d
DemF <viktop.a@yandex.ru>
2022-06-28 22:28:39 +0300

    add cmake base config, add SimpleEngineCore, add SimpleEngineCore

---
+++ SimpleEngineCore/src/SimpleEngineCore/Utils/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "SimpleEngineCore/Utils/test.hpp"
+
+namespace SimpleEngine {
+
+	void sayHello() {
+		std::cout << "Hello from Simple Engine Core" << std::endl;
+	}
+
+}
---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <SimpleEngineCore/Utils/test.hpp>
+
+int main()
+{
+	std::cout << "Hello from Simple Engine Editor"<<std::endl;
+	SimpleEngine::sayHello();
+	std::cin.get();
+}
